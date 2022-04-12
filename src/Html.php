af3dface6c27cebbc80200798f37bc313dfc4bb9
Nguyễn Văn Hiệp <nguyenhiepvan.bka@gmail.com>
2022-04-12 14:17:12 +0700

    update regexes

--- src/Html.php
+++ src/Html.php
@@ -120,20 +120,20 @@ function parseKeywords(string $main): array
 
 function stripVN(string $str): string
 {
-    $str = preg_replace("/(à|á|ạ|ả|ã|â|ầ|ấ|ậ|ẩ|ẫ|ă|ằ|ắ|ặ|ẳ|ẵ)/u", 'a', $str);
-    $str = preg_replace("/(è|é|ẹ|ẻ|ẽ|ê|ề|ế|ệ|ể|ễ)/u", 'e', $str);
-    $str = preg_replace("/(ì|í|ị|ỉ|ĩ)/u", 'i', $str);
-    $str = preg_replace("/(ò|ó|ọ|ỏ|õ|ô|ồ|ố|ộ|ổ|ỗ|ơ|ờ|ớ|ợ|ở|ỡ)/u", 'o', $str);
-    $str = preg_replace("/(ù|ú|ụ|ủ|ũ|ư|ừ|ứ|ự|ử|ữ)/u", 'u', $str);
-    $str = preg_replace("/(ỳ|ý|ỵ|ỷ|ỹ)/u", 'y', $str);
+    $str = preg_replace("/(àáạảãâầấậẩẫăằắặẳẵ)/u", 'a', $str);
+    $str = preg_replace("/(èéẹẻẽêềếệểễ)/u", 'e', $str);
+    $str = preg_replace("/(ìíịỉĩ)/u", 'i', $str);
+    $str = preg_replace("/(òóọỏõôồốộổỗơờớợởỡ)/u", 'o', $str);
+    $str = preg_replace("/(ùúụủũưừứựửữ)/u", 'u', $str);
+    $str = preg_replace("/(ỳýỵỷỹ)/u", 'y', $str);
     $str = preg_replace("/(đ)/u", 'd', $str);
 
-    $str = preg_replace("/(À|Á|Ạ|Ả|Ã|Â|Ầ|Ấ|Ậ|Ẩ|Ẫ|Ă|Ằ|Ắ|Ặ|Ẳ|Ẵ)/u", 'A', $str);
-    $str = preg_replace("/(È|É|Ẹ|Ẻ|Ẽ|Ê|Ề|Ế|Ệ|Ể|Ễ)/u", 'E', $str);
-    $str = preg_replace("/(Ì|Í|Ị|Ỉ|Ĩ)/u", 'I', $str);
-    $str = preg_replace("/(Ò|Ó|Ọ|Ỏ|Õ|Ô|Ồ|Ố|Ộ|Ổ|Ỗ|Ơ|Ờ|Ớ|Ợ|Ở|Ỡ)/u", 'O', $str);
-    $str = preg_replace("/(Ù|Ú|Ụ|Ủ|Ũ|Ư|Ừ|Ứ|Ự|Ử|Ữ)/u", 'U', $str);
-    $str = preg_replace("/(Ỳ|Ý|Ỵ|Ỷ|Ỹ)/u", 'Y', $str);
+    $str = preg_replace("/(ÀÁẠẢÃÂẦẤẬẨẪĂẰẮẶẲẴ)/u", 'A', $str);
+    $str = preg_replace("/(ÈÉẸẺẼÊỀẾỆỂỄ)/u", 'E', $str);
+    $str = preg_replace("/(ÌÍỊỈĨ)/u", 'I', $str);
+    $str = preg_replace("/(ÒÓỌỎÕÔỒỐỘỔỖƠỜỚỢỞỠ)/u", 'O', $str);
+    $str = preg_replace("/(ÙÚỤỦŨƯỪỨỰỬỮ)/u", 'U', $str);
+    $str = preg_replace("/(ỲÝỴỶỸ)/u", 'Y', $str);
     return preg_replace("/(Đ)/u", 'D', $str);
 }
 
